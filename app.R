a16325ed9e3744f2727f80610b18b536e1cace89
David Currie <davidcurrie2001@yahoo.co.uk>
2025-04-09 12:55:02 +0100

    Added filter for open/closed, and handled problems loading remote data more gracefully

--- app.R
+++ app.R
@@ -13,16 +13,22 @@ library(shinyWidgets)
 # Note if you don't use "shinyUI" then the networkD3 graphs won't work properly 
 ui <- shinyUI(fluidPage(
   
+  theme = bs_theme(bootswatch = "lumen"),
+  
   # Application title
   titlePanel("ICES DIG Challenges and Opportunities"),
-  
-  # use default Bootstrap styles
-  theme = bs_theme(bootswatch = "lumen"),
+  hr(),
 
   sidebarLayout(
     
     # Controls 
     sidebarPanel(
+      selectInput("openClosedSelect",
+                  label="Open/closed",
+                  choices=c("Open","Closed"),
+                  selected="Open",
+                  multiple = TRUE
+      ),
       selectInput("ChallOppSelect",
                   label="Type",
                   choices=c("Challenge","Opportunity","Issue"),
@@ -128,6 +134,15 @@ ui <- shinyUI(fluidPage(
                  fluidRow(
                    column(4,
                     checkboxInput("networkZoom", "Allow zoom?", FALSE)
+                   ),
+                   column(4,
+                     sliderInput( 
+                        "networkNodesize", 
+                        "Node size", 
+                        value = 1, 
+                        min = 1, 
+                        max = 100 
+                     )
                    )
                  )
               )
@@ -139,12 +154,15 @@ ui <- shinyUI(fluidPage(
 #### Server ####
 server <- function(input, output, session) {
   
-  useLocalData <- TRUE
+  # Can use local data when testing - set this to FALSE before deploying though
+  useLocalData <- FALSE
   queryDate <- NA
-  
   defaultPalette <- list("Challenge"="#FF0000",
                          "Opportunity"="#008000",
                          "Issue"="#0000FF")
+  
+  # Reactive value to control a plot's height
+  IssuePlotHeight <- reactiveVal(value = 550)
 
   # Show a spinner whilst app data is being loaded
   showPageSpinner()
@@ -152,12 +170,30 @@ server <- function(input, output, session) {
   # load palettes
   AllPalettes <- read.csv(file="data/palettes.csv")
   
-  # load the data
-  if (useLocalData){
-    load("data/DIG_Issues.RData")
-  } else {
+  # load the issue data
+  remoteDataSuccess <- FALSE
+  if (!useLocalData){
     githubDataURL <- "https://raw.githubusercontent.com/davidcurrie2001/DIG_Challenges/refs/heads/master/data/DIG_Issues.RData"
-    load(url(githubDataURL))
+    # try and load the data - set the return value to FALSE if we run into problems
+    remoteDataSuccess <- tryCatch(
+      expr = {
+        load(url(githubDataURL))
+        remoteSuccess <- TRUE
+      },
+      error = function(e){ 
+        print(e)
+        remoteSuccess <- FALSE
+      },
+      warning = function(w){ 
+        print(w)
+        remoteSuccess <- FALSE
+      }
+    )
+  }
+  
+  # Load the app's local data if either we want to or we need to (problems loading remote data)
+  if (useLocalData | (remoteDataSuccess == FALSE)){
+    load("data/DIG_Issues.RData")
   }
   
   # Hide the spinner once data is loaded
@@ -166,7 +202,7 @@ server <- function(input, output, session) {
   # When was the date extracted?
   queryDate <- DIG_Issues[["queryDate"]]
   
-  
+  # Create the AllIssues frame and add a column
   AllIssues <- DIG_Issues[["issues"]]
   AllIssues$Type <- ifelse(AllIssues$Challenge == TRUE, 'Challenge', 
                                   ifelse(AllIssues$Opportunity == TRUE, 'Opportunity', 'Issue'))
@@ -196,7 +232,20 @@ server <- function(input, output, session) {
     
     myIssuesFiltered <- AllIssues
     
-    myIssuesFiltered <- myIssuesFiltered[myIssuesFiltered$state == "OPEN",]
+    mySeverity <- input$severitySelect
+    
+    # Filter by open/closed
+    if (length(input$openClosedSelect) > 0){
+      myIssuesFiltered <- myIssuesFiltered[tolower(myIssuesFiltered$state)  %in% tolower(input$openClosedSelect),]
+      if ("closed" %in% tolower(input$openClosedSelect)){
+        # if we're including closed issues then we'll also allow "Done" as a status
+        mySeverity <- c(mySeverity,"Done")
+      }
+    } else {
+      myIssuesFiltered <- myIssuesFiltered[0==1,]
+    }
+    
+    #myIssuesFiltered <- myIssuesFiltered[myIssuesFiltered$state == "OPEN",]
     
     # Filter by the type
     if (length(input$ChallOppSelect) >0) {
@@ -206,10 +255,10 @@ server <- function(input, output, session) {
     }
     
     # Filter by the severity
-    if (length(input$severitySelect) >0){
+    if (length(mySeverity) >0){
       # "Issues" don't have severity so we won't apply this filter to them
       myIssuesFiltered <- myIssuesFiltered[myIssuesFiltered$Type == "Issue" | 
-                                             myIssuesFiltered$projectField %in% input$severitySelect,]
+                                             myIssuesFiltered$projectField %in% mySeverity,]
     } else {
       myIssuesFiltered <- myIssuesFiltered[0==1,]
     }
@@ -243,10 +292,18 @@ server <- function(input, output, session) {
     
   })
   
-  # Update the issue number select input based on the other inputs
+  
+  # Update i) issue number select input and ii) plot height variable based on the other inputs
   observe({
     fi <- FilterIssues_Step1()
+    
+    # Update the issue filter based on the other drop-downs
     updatePickerInput(session, "issuePicker", choices = fi$number, selected = fi$number)
+    
+    # Set the value of IssuePlotHeight based on how many records there are
+    myHeight <- nrow(fi) * 30
+    if (myHeight < 550) myHeight <- 550
+    IssuePlotHeight(myHeight)
   })
   
   # Join issues to their linked issues
@@ -256,13 +313,10 @@ server <- function(input, output, session) {
     #issues <- myIssuesFiltered
     myLinks <- IssueLinks
     
+    # (includes Open and Closed issues)
     otherIssues <- AllIssues
     names(otherIssues) <- paste("crossRefBy_",names(otherIssues),sep="")
-    # TODO Only get the links to issues that aren't Challenges/Opportunities (includes Open and Closed issues)
-    #otherIssues <- otherIssues[otherIssues$Challenge == FALSE & otherIssues$Opportunity == FALSE,]
     otherIssues <- inner_join(myLinks,otherIssues,by=c("crossRefById"="crossRefBy_id"))
-    #names(otherIssues)
-    #otherIssues <- otherIssues[,c("id","crossRefById")]
     myData <- left_join(issues,otherIssues,by="id")
     myData
   }
@@ -287,75 +341,28 @@ server <- function(input, output, session) {
     
   })
   
-
-  output$force <- renderForceNetwork({
+  
+  # Display the Challenges/Opportunities in a table
+  output$summaryTable <- renderDataTable({
     
-    myData <- JoinIssuesWithLinks()
-    #names(myData)
-    
-    # the validate function in Shiny was being masked - so use explicit package name
-    shiny::validate(need(nrow(myData) > 0, "No data to display"))
-    
-    # Need to make a single list of nodes
-    # First get the challenge/oppotunities
-    Nodes <- myData[,c("id","number","fullName","Type")]
-    names(Nodes) <- c("id","number","Name","Type")
-    # then get the cross-references nodes
-    crossRef <- myData[!is.na(myData$crossRefById),c("crossRefById","crossRefBy_number","crossRefBy_fullName","crossRefBy_Type")]
-    names(crossRef) <- c("id","number","Name","Type")
-    # then combind them
-    Nodes <- rbind(Nodes,crossRef)
-    # the de-dupe, order them, and generate an ID starting at 0
-    Nodes <- Nodes[!duplicated(Nodes),]
-    Nodes <- Nodes[order(Nodes$number),]
-    Nodes$ID <- seq(0,nrow(Nodes)-1)
-    rownames(Nodes) <- NULL
-    
-    # Shorten the names for display
-    Nodes$Name <- substr(Nodes$Name, 1, input$networkNameLength)
-    
-    # Now need to create the Links, with references to the numeric ID we just defined for each node
-    Links <- myData
-    Links <- dplyr::left_join(Links,Nodes,by=c("id"="id"))
-    Links$source <- Links$ID
-    Links$ID <- NULL
-    #Links$Group <- NULL
-    Links <- dplyr::left_join(Links,Nodes,by=c("crossRefById"="id"))
-    Links$target <- Links$ID
-    Links <- Links[,c("source","target")]
-    Links[is.na(Links$target),"target"] <- Links[is.na(Links$target),"source"]
-    
-    
-    # Define a custom palette - the string should look something like this
-    #ColourScale <- 'd3.scaleOrdinal()
-    #        .domain(["Challenge", "Opportunity", "Issue" ])
-    #       .range(["#FF0000", "#008000", "#0000FF"]);'
-    ColourScale <- paste0("d3.scaleOrdinal().domain([",
-           paste("'",names(appPalette()),"'", sep="", collapse = ","),
-           "]).range([",
-           paste("'",appPalette(),"'", sep="", collapse = ","),
-           "]);")
-
-    forceNetwork(Links = Links, 
-                 Nodes = Nodes, 
-                 NodeID = "Name",
-                 Source = "source", 
-                 Target = "target", 
-                 Group = "Type", 
-                 opacity = 1.0,
-                 fontFamily = "serif",
-                 colourScale = JS(ColourScale),
-                 opacityNoHover = 1, # always show names
-                 zoom = input$networkZoom,
-                 legend = TRUE,
-                 fontSize = input$networkFont,  
-                 linkDistance = input$networkDistance,
-                 charge=input$networkCharge)
-    
-  })
+    data <- JoinIssuesWithLinks() %>%
+      dplyr::mutate(fullNameAndLink =  paste0("<a href='",url, "'  target='_blank'>",fullName,"</a>")) %>%
+      dplyr::mutate(crossRefNumberAndLink =  ifelse(is.na(crossRefBy_number),
+                                                    "",
+                                                    paste0("<a href='",crossRefBy_url, "'  target='_blank'>",crossRefBy_number,"</a>"))) %>%
+      dplyr::arrange(desc(fullNameAndLink),crossRefNumberAndLink) %>%
+      dplyr::group_by(fullNameAndLink,Type) %>%
+      dplyr::mutate(crossRefURLs = paste0(crossRefNumberAndLink, collapse = ", ")) %>%
+      dplyr::select(fullNameAndLink,Type,crossRefURLs) %>%
+      unique() %>%
+      dplyr::arrange(desc(fullNameAndLink)) %>%
+      dplyr::rename(Name = fullNameAndLink, "Related Issues" = crossRefURLs)
+  }, rownames= FALSE, escape = FALSE, options = list(dom = 'tp'))
+  
   
   # Display a plot showing the number of related issues
   output$relatedIssuePlot <- renderPlot({
+    
     
     myData <- JoinIssuesWithLinks()
     
@@ -383,24 +390,79 @@ server <- function(input, output, session) {
       labs(x = "", y="Number of related issues") +
       rotate()
     
-  })
+  }, height = function() IssuePlotHeight()) # dynamically set the height
   
-  # Display the Challenges/Opportunities
-  output$summaryTable <- renderDataTable({
+  
+  # Plot the ofrce network graph
+  output$force <- renderForceNetwork({
     
-    data <- JoinIssuesWithLinks() %>%
-      dplyr::mutate(fullNameAndLink =  paste0("<a href='",url, "'  target='_blank'>",fullName,"</a>")) %>%
-      dplyr::mutate(crossRefNumberAndLink =  ifelse(is.na(crossRefBy_number),
-                                                    "",
-                                                    paste0("<a href='",crossRefBy_url, "'  target='_blank'>",crossRefBy_number,"</a>"))) %>%
-      dplyr::arrange(desc(fullNameAndLink),crossRefNumberAndLink) %>%
-      dplyr::group_by(fullNameAndLink,Type) %>%
-      dplyr::mutate(crossRefURLs = paste0(crossRefNumberAndLink, collapse = ", ")) %>%
-      dplyr::select(fullNameAndLink,Type,crossRefURLs) %>%
-      unique() %>%
-      dplyr::arrange(desc(fullNameAndLink)) %>%
-      dplyr::rename(Name = fullNameAndLink, Related = crossRefURLs)
-  }, rownames= FALSE, escape = FALSE, options = list(dom = 'tp'))
+    myData <- JoinIssuesWithLinks()
+    #names(myData)
+    
+    # the validate function in Shiny was being masked - so use explicit package name
+    shiny::validate(need(nrow(myData) > 0, "No data to display"))
+    
+    # Need to make a single list of nodes
+    # First get the challenge/oppotunities
+    Nodes <- myData[,c("id","number","fullName","Type")]
+    names(Nodes) <- c("id","number","Name","Type")
+    # then get the cross-references nodes
+    crossRef <- myData[!is.na(myData$crossRefById),c("crossRefById","crossRefBy_number","crossRefBy_fullName","crossRefBy_Type")]
+    names(crossRef) <- c("id","number","Name","Type")
+    # then combind them
+    Nodes <- rbind(Nodes,crossRef)
+    # the de-dupe, order them, and generate an ID starting at 0
+    Nodes <- Nodes[!duplicated(Nodes),]
+    Nodes <- Nodes[order(Nodes$number),]
+    Nodes$ID <- seq(0,nrow(Nodes)-1)
+    
+    # size the nodes based on input
+    Nodes$nodeSize <- input$networkNodesize
+    rownames(Nodes) <- NULL
+    
+    # Shorten the names for display
+    Nodes$Name <- substr(Nodes$Name, 1, input$networkNameLength)
+    
+    # Now need to create the Links, with references to the numeric ID we just defined for each node
+    Links <- myData
+    Links <- dplyr::left_join(Links,Nodes,by=c("id"="id"))
+    Links$source <- Links$ID
+    Links$ID <- NULL
+    #Links$Group <- NULL
+    Links <- dplyr::left_join(Links,Nodes,by=c("crossRefById"="id"))
+    Links$target <- Links$ID
+    Links <- Links[,c("source","target")]
+    Links[is.na(Links$target),"target"] <- Links[is.na(Links$target),"source"]
+    
+    
+    # Define a custom palette - the string should look something like this
+    #ColourScale <- 'd3.scaleOrdinal()
+    #        .domain(["Challenge", "Opportunity", "Issue" ])
+    #       .range(["#FF0000", "#008000", "#0000FF"]);'
+    ColourScale <- paste0("d3.scaleOrdinal().domain([",
+                          paste("'",names(appPalette()),"'", sep="", collapse = ","),
+                          "]).range([",
+                          paste("'",appPalette(),"'", sep="", collapse = ","),
+                          "]);")
+    
+    forceNetwork(Links = Links, 
+                 Nodes = Nodes, 
+                 NodeID = "Name",
+                 Source = "source", 
+                 Target = "target", 
+                 Group = "Type", 
+                 opacity = 1.0,
+                 fontFamily = "serif",
+                 colourScale = JS(ColourScale),
+                 opacityNoHover = 1, # always show names
+                 zoom = input$networkZoom,
+                 legend = TRUE,
+                 Nodesize = "nodeSize",
+                 fontSize = input$networkFont,  
+                 linkDistance = input$networkDistance,
+                 charge=input$networkCharge)
+    
+  })
   
 }
 
